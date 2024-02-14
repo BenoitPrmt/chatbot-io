c2aac7175ae2ebabcbef7756bdba151b5d4256f0
Raxuis <raclot.raphael@gmail.com>
2024-02-14 20:14:54 +0100

    Feat(front) : Added chat's design

--- src/views/home.js
+++ src/views/home.js
@@ -1,8 +1,135 @@
 export default () => (`
-    <section class="row messages">
+    <section class="row chat">
     <div class="col-3 left-side">
-    <h3 class="text-light">Test</h3>
+    <h3 class="text-light">Bots' Name</h3>
+    <ul class="list-group gap-3 pt-3">
+    <li class="list-group-item">
+    <div class="d-flex flex-column flex-lg-row gap-3 align-items-center">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+    <h5 class="text-black pt-0 pt-lg-1">Bot 1</h5>
+    </div></li><li class="list-group-item">
+    <div class="d-flex flex-column flex-lg-row gap-3 align-items-center">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+    <h5 class="text-black pt-0 pt-lg-1">Bot 1</h5>
+    </div></li><li class="list-group-item">
+    <div class="d-flex flex-column flex-lg-row gap-3 align-items-center">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+    <h5 class="text-black pt-0 pt-lg-1">Bot 1</h5>
+    </div></li><li class="list-group-item">
+    <div class="d-flex flex-column flex-lg-row gap-3 align-items-center">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+    <h5 class="text-black pt-0 pt-lg-1">Bot 1</h5>
+    </div></li></ul>
     </div>
-    <div class="col-9 right-side"></div>
-    </section>
+    <div class="col-9 right-side">
+
+    <div class="d-flex justify-content-end">
+    <div class="card">
+    <div class="card-header justify-content-end">
+    <h5>Bot 1</h5>
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+    </div>
+        <div class="card-body">
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div>
+
+<div class="d-flex justify-content-start">
+    <div class="card">
+    <div class="card-header">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+        <h5>Benoît</h5>
+    </div>
+        <div class="card-body">
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div>
+<div class="d-flex justify-content-start">
+    <div class="card">
+    <div class="card-header">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+        <h5>Benoît</h5>
+    </div>
+        <div class="card-body">
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div>
+<div class="d-flex justify-content-start">
+    <div class="card">
+    <div class="card-header">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+        <h5>Benoît</h5>
+    </div>
+        <div class="card-body">
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div>
+<div class="d-flex justify-content-start">
+    <div class="card">
+    <div class="card-header">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+        <h5>Benoît</h5>
+    </div>
+        <div class="card-body">
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div>
+<div class="d-flex justify-content-start">
+    <div class="card">
+    <div class="card-header">
+    <img class="avatar rounded-circle"
+                 src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                 alt="">
+        <h5>Benoît</h5>
+    </div>
+        <div class="card-body">
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        </div>
+    </div>
+</div><div class="d-flex justify-content-start">
+<div class="card">
+<div class="card-header">
+<img class="avatar rounded-circle"
+             src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+             alt="">
+    <h5>Benoît</h5>
+</div>
+    <div class="card-body">
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    </div>
+</div>
+</div>
+<div class="messages">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Type something..." aria-label="Type something..."
+                   aria-describedby="button-addon2">
+            <button class="btn btn-dark" type="button" id="button-addon2"><i class="fa-solid fa-paper-plane"></i></button>
+        </div>
+    </div>
+</div>
+</div>
+</section>
 `);
