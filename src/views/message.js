3ac4127cce77e4bacf18b490909a4acc0a97b465
BenoitPrmt <benoitparmentier45@gmail.com>
2024-02-15 11:05:05 +0100

    feat(front): optim code

---
+++ src/views/message.js
@@ -0,0 +1,21 @@
+export default (data) => {
+  const {
+    sender,
+    content
+  } = data;
+  return `
+    <div class="d-flex ${sender === 'User' ? 'justify-content-end' : 'justify-content-start'}">
+      <div class="card">
+        <div class="card-header ${sender === 'User' ? 'justify-content-end' : 'justify-content-start'}">
+          <h5>${sender}</h5>
+          <img class="avatar rounded-circle"
+                       src="https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2022/12/tortue-marine-scaled.jpg"
+                       alt="">
+        </div>
+        <div class="card-body">
+            <p class="card-text">${content}</p>
+        </div>
+      </div>
+    </div>
+    `;
+};
