d4d109b2f2664a013ca7a9789b65f1d803ac0e94
Raxuis <raclot.raphael@gmail.com>
2024-02-16 19:32:09 +0100

    Feat(en) : switch welcome page language to english

--- src/views/welcome.js
+++ src/views/welcome.js
@@ -22,16 +22,16 @@ export default () => {
       }
     });
   });
-  return `<h1 class="text-light">Bienvenue ici !</h1>
+  return `<h1 class="text-light">Welcome here !</h1>
   <div class="d-flex flex-column align-items-center gap-4 pt-5">
   <img class="login-avatar rounded-circle" src="https://source.boringavatars.com/" alt="">
   <div class="d-flex flex-column w-100 pt-5 align-items-center">
-  <label class="text-light">Nom d'utilisateur :</label>
+  <label class="text-light">Username :</label>
   <div class="input-group mt-1">
-  <input type="text" class="form-control input-message-content" id="username" placeholder="Entrez votre nom d'utilisateur..." aria-label="Entrez votre nom d'utilisateur..."
+  <input type="text" class="form-control input-message-content" id="username" placeholder="Enter your username..." aria-label="Enter your username..."
   aria-describedby="button-addon2">
   </div>
-  <button class="btn btn-primary mt-2" type="button" id="submitBtn">Discuter avec les robots</button>
+  <button class="btn btn-primary mt-2" type="button" id="submitBtn">Chat with the bots</button>
   </div>
   </div>`;
 };
