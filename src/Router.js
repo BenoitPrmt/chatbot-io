000bb40d024dc383ed3b86c03d2d26dd3e40e913
Raxuis <raclot.raphael@gmail.com>
2024-02-16 12:11:55 +0100

    Style(chat cards) : removed letter spacing

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import PageNotFound from './controllers/404';
-
+// 👇 uncomment to import
+/* import Welcome from './controllers/Welcome'; */
 const Router = class {
   constructor(routes = []) {
     this.path = window.location.pathname;
@@ -14,11 +15,29 @@ const Router = class {
     this.run();
   }
 
+  verifLogged() {
+    if (localStorage.getItem('username')) {
+      return true;
+    }
+    return false;
+  }
+
   startController() {
     let ifExist = false;
 
     for (let i = 0; i < this.routes.length; i += 1) {
       const route = this.routes[i];
+
+      // 👇 uncomment to check if the user is already logged in
+
+      /* if (!this.verifLogged()) {
+        route.url = '/welcome';
+        route.controller = Welcome;
+        const Controller = route.controller;
+        new Controller(this.params);
+        ifExist = true;
+        break;
+      } */
 
       if (route.url === this.path) {
         const Controller = route.controller;
