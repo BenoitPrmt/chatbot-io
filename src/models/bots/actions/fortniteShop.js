59f76a240accf936f0e556ee0613e739120820ed
Raxuis <raclot.raphael@gmail.com>
2024-02-29 14:52:04 +0100

    Fix(bundle shop handling) : Fixed the handling oh the bundles shown with ftnshop

--- src/models/bots/actions/fortniteShop.js
+++ src/models/bots/actions/fortniteShop.js
@@ -19,9 +19,7 @@ const FortniteShop = {
         message: `
         The item shop is ${datas.entries.length} items long.<br/>
         There is : <br/>${datas.entries.map((entry) => `
-        ${entry.bundle?.name ? `Bundle : ${entry.bundle.name}<br/>` : ''}
-        Regular Price : ${entry.regularPrice}<br/>
-        Final Price : ${entry.finalPrice}<br/>
+        ${entry.bundle?.name ? `Bundle : ${entry.bundle.name}<br/>Regular Price : ${entry.regularPrice}<br/>` : `Final Price : ${entry.finalPrice}<br/>`}
         ${entry.items.map((item) => `
             <img src='${item.images.icon}' width="40" height="40" class="bg-dark rounded p-1"/> ${item.name} | ${item.description}`).join('<br /><br/>')}
             `).join('<br /><br/>')}`
