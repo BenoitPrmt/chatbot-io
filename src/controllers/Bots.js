0813dadf55f1ff338bdb64bd255ecd17993f7f75
Raxuis <raclot.raphael@gmail.com>
2024-04-04 10:47:34 +0200

    feat(bots): :card_file_box: Make bot page depending database

--- src/controllers/Bots.js
+++ src/controllers/Bots.js
@@ -1,15 +1,36 @@
+import axios from 'axios';
 import viewNav from '../views/nav';
 import viewBots from '../views/bots';
-import entities from '../data/entitiesData';
+import Bot from '../models/bots';
 
 const Bots = class {
   constructor(params) {
     this.el = document.querySelector('#root');
     this.params = params;
 
-    this.bots = entities;
+    this.startChat();
+  }
+
+  async startChat() {
+    this.bots = await this.getBotsEntitiesInDatabase('1');
 
     this.run();
+  }
+
+  async getBotsEntitiesInDatabase() {
+    const apiUrlPhpBots = 'http://localhost:8080/bots';
+
+    const options = {
+      method: 'GET',
+      url: apiUrlPhpBots
+    };
+    const response = await axios.request(options);
+    try {
+      this.bots = response.data.map((entity) => new Bot(entity));
+    } catch (error) {
+      return error;
+    }
+    return response.data;
   }
 
   render() {
