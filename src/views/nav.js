305141263d6067c1143536afc9c32d5ef62e2564
Raxuis <raclot.raphael@gmail.com>
2024-04-05 16:37:32 +0200

    feat(navbar): :art: Add active property that makes the links corresponding to the url go blue

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,13 +1,19 @@
 import { robotIcon } from '../assets/index';
 
-export default () => (`
-<nav class="navbar navbar-expand-lg py-2">
-  <div class="container-fluid d-flex ps-5">
-    <div class="d-flex gap-3">
-    <a href='/'><img src="${robotIcon}" alt="Robot Icon"></a>
-    <a class="nav-link" href="/">Chatbot IO</a>
-    <a class="nav-link" href="/bots">Commands</a>
-    </div>
-  </div>
-</nav>
-`);
+export default () => {
+  const currentPath = window.location.pathname;
+
+  const isActive = (path) => (currentPath === path ? 'active' : '');
+
+  return `
+    <nav class="navbar navbar-expand-lg py-2">
+      <div class="container-fluid d-flex ps-5">
+        <div class="d-flex gap-3">
+          <a href='/' style="text-decoration: none;"><img src="${robotIcon}" alt="Robot Icon"></a>
+          <a class="nav-link ${isActive('/')}" href="/">Chatbot IO</a>
+          <a class="nav-link ${isActive('/bots')}" href="/bots">Commands</a>
+        </div>
+      </div>
+    </nav>
+  `;
+};
